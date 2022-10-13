d43e0d55e8fb07bc96d665765b47dda5abb340ad
pritamsharma45 <69457813+pritamsharma45@users.noreply.github.com>
2022-10-13 16:00:49 +0530

    2nd Commit

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,20 +1,38 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
+    title: 'Task Manager',
+    description: `This tool lets accountants manage their client's tasks.`,
     imgSrc: '/static/images/google.png',
     href: 'https://www.google.com',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Form to Collect Clients information',
+    description: `This tool lets accountants manage their client's tasks.`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://www.google.com',
   },
+  {
+    title: 'Invoicing Application linked to Google Sheets',
+    description: `Generate invoices at breeze.`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://www.google.com',
+  },
+  // {
+  //   title: 'A Search Engine',
+  //   description: `What if you could look up any information in the world? Webpages, images, videos
+  //   and more. Google has many features to help you find exactly what you're looking
+  //   for.`,
+  //   imgSrc: '/static/images/google.png',
+  //   href: 'https://www.google.com',
+  // },
+  // {
+  //   title: 'The Time Machine',
+  //   description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+  //   to the desired date and press "Go". No more worrying about lost keys or
+  //   forgotten headphones with this simple yet affordable solution.`,
+  //   imgSrc: '/static/images/time-machine.jpg',
+  //   href: '/blog/the-time-machine',
+  // },
 ]
 
 export default projectsData
