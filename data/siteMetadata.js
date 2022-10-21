073dce284afb503e0a5d88de451dd21575895f92
pritamsharma45 <69457813+pritamsharma45@users.noreply.github.com>
2022-10-21 22:30:47 +0530

    2nd commit

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 const siteMetadata = {
   title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  author: 'Pritam Sharma',
+  headerTitle: 'LeverTech',
+  description: 'A blog to showcase productivity tools.',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
