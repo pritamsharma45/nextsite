85f96d9648cbfa730b70d17083afc938e9c4c69e
pritamsharma45 <69457813+pritamsharma45@users.noreply.github.com>
2022-12-19 17:11:05 +0530

    comments and newsletter subscribing features added

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -30,14 +30,14 @@ const siteMetadata = {
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
     // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
+    provider: 'emailoctopus',
   },
   comment: {
     // If you want to use a commenting system other than giscus you have to add it to the
     // content security policy in the `next.config.js` file.
     // Select a provider and use the environment variables associated to it
     // https://vercel.com/docs/environment-variables
-    provider: 'giscus', // supported providers: giscus, utterances, disqus
+    provider: 'utterances', // supported providers: giscus, utterances, disqus
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
@@ -67,11 +67,11 @@ const siteMetadata = {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://utteranc.es/
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
-      issueTerm: '', // supported options: pathname, url, title
+      issueTerm: 'pathname', // supported options: pathname, url, title
       label: '', // label (optional): Comment 💬
       // theme example: github-light, github-dark, preferred-color-scheme
       // github-dark-orange, icy-dark, dark-blue, photon-dark, boxy-light
-      theme: '',
+      theme: 'github-dark-orange',
       // theme when dark mode
       darkTheme: '',
     },
---
+++ todo.md
@@ -0,0 +1,7 @@
+ Integrate the following 
+1. Payment 
+2. comments
+3. News letter
+4. Social Networking sites
+
+
