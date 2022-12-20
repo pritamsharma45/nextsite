4b26099a71cd2ebde5e4e722b07dd57b36401bf4
pritamsharma45 <69457813+pritamsharma45@users.noreply.github.com>
2022-12-20 11:48:05 +0530

    Disqus and mailchimp added

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -30,14 +30,14 @@ const siteMetadata = {
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
     // Please add your .env file and modify it according to your selection
-    provider: 'emailoctopus',
+    provider: 'mailchimp',
   },
   comment: {
     // If you want to use a commenting system other than giscus you have to add it to the
     // content security policy in the `next.config.js` file.
     // Select a provider and use the environment variables associated to it
     // https://vercel.com/docs/environment-variables
-    provider: 'utterances', // supported providers: giscus, utterances, disqus
+    provider: 'disqus', // supported providers: giscus, utterances, disqus
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
