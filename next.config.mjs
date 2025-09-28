33ddc629a671c409f56f2f489967544e7527a5cd
pritamsharma45 <notifications.borl@gmail.com>
2025-09-28 13:39:11 +0530

    dfgsfd

--- next.config.mjs
+++ next.config.mjs
@@ -8,11 +8,11 @@ const withBundleAnalyzer = bundleAnalyzer({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is www.google-analytics.com www.googletagmanager.com;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
-  connect-src *;
+  connect-src * www.google-analytics.com www.googletagmanager.com ssl.google-analytics.com;
   font-src 'self';
   frame-src giscus.app
 `
